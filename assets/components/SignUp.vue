5f63c0b6e4825e5ce49be68e2dab23c369c5c35c
temagor <temagor2015@gmail.com>
2022-08-20 11:20:44 +0300

    signUp form with base api point

--- assets/components/SignUp.vue
+++ assets/components/SignUp.vue
@@ -7,15 +7,25 @@
   </button>
   <div v-if="isVisible">
     <div class="form form_popup">
+      <div
+        v-if="message"
+        class="form__message"
+        :class="{
+          form__message_success: success,
+          form__message_error: !success,
+        }"
+      >
+        {{ message }}
+      </div>
       <form action="#" method="post" @submit.prevent="register">
         <input v-model="user.login" type="text" placeholder="Enter login" />
         <input
-          v-model="needPasswordProtection"
+          v-model="user.needPasswordProtection"
           type="checkbox"
           id="switch"
         /><label for="switch">Toggle</label>
         <Transition name="slide-fade">
-          <div v-if="needPasswordProtection">
+          <div v-if="user.needPasswordProtection">
             <input
               v-model="user.password"
               type="password"
@@ -37,14 +47,18 @@
 <script>
 import { Transition } from "vue";
 import axios from "axios";
+
 export default {
   components: { Transition },
   data() {
     return {
+      message: null,
+      success: false,
+      errors: {},
       isVisible: false,
-      needPasswordProtection: false,
       user: {
         login: "",
+        needPasswordProtection: false,
         password: "!@ChangeMe!",
         confirmPassword: "!@ChangeMe!",
       },
@@ -54,8 +68,16 @@ export default {
     toggleVisible() {
       this.isVisible = !this.isVisible;
     },
-    register(e) {
-      axios.post("/api/user/create", this.user);
+    register() {
+      axios.post("/user/actions/api/sign-up", this.user).then((response) => {
+        this.success = response.data.success;
+        this.message = response.data.message;
+        if (response.data.success) {
+          setTimeout(() => {
+            window.location.reload();
+          }, 1500);
+        }
+      });
     },
   },
 };
