a3117003454640686de7dced85fdc0628c83ab10
temagor <temagor2015@gmail.com>
2022-09-04 11:28:31 +0300

    add profile empty modal

--- assets/components/authentificated/Profile.vue
+++ assets/components/authentificated/Profile.vue
@@ -1,13 +1,37 @@
 <template>
-  <button class="button navigation__button button_profile">Profile</button>
+  <button class="button navigation__button button_profile" @click="show">
+    Profile
+  </button>
+  <div v-if="isVisible" class="modal-wrapper" @click="hide">
+    <div class="content" @click.stop="">
+      <div class="title">Profile</div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapState } from "pinia";
 import { useUserStore } from "../../stores/user";
 export default {
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
   computed: {
     ...mapState(useUserStore, ["user"]),
+  },
+  methods: {
+    show() {
+      if (!this.isVisible) {
+        this.isVisible = true;
+      }
+    },
+    hide() {
+      if (this.isVisible) {
+        this.isVisible = false;
+      }
+    },
   },
 };
 </script>
